37143f67d5b9c11e4aae2de7e81090820296c98b
Dr Van Camp <john@van-camp.com>
2018-05-21 09:19:50 -0700

    update defl() w/tidyverse & update notes

--- R/defl.R
+++ R/defl.R
@@ -2,23 +2,19 @@
 # load my basic toolset
 #
 #' defl() load my default libraries
-#' @keywords default library: dplyr, ggplot2, reshape2, tidyr, data.table, magrittr, readr
+#' @keywords default library: data.table, magrittr,
+#'     tydyverse(ggplot2,dplyr,tidyr,readr,purrr,tibble,stringr,forcats)
 #' @export
 #' @examples
 #' defl()
 
-
 defl <-function(){
     f = file()
     sink(file=f) ## silence upcoming output using anonymous file connection
-    library(dplyr)
-    library(ggplot2)
+    library(tidyverse)
     library(reshape2)
-    library(tidyr)
     library(data.table)
     library(magrittr)
-    library(readr)
-    library(purrr)
     # library(sqldf)
     sink() ## undo silencing
     close(f)
